4111372820c50c4bacc36082fd433d8be81d8ed2
Bruno Goulart <bruno-goulart1@live.com>
2023-06-29 21:53:50 -0300

    #Ajustado envio de imagens via rest

--- TechChallenge.Ui.Razor/Pages/Products.cshtml.cs
+++ TechChallenge.Ui.Razor/Pages/Products.cshtml.cs
@@ -22,45 +22,44 @@ namespace TechChallenge.Ui.Razor.Pages
 
         public async Task<IActionResult> OnPostAsync()
         {
-            var stream = new MemoryStream();
-            FileInput.CopyTo(stream);
-            byte[] bytes2 = stream.ToArray();
-
+            var httpClient = new HttpClient();
             var url = "https://localhost:7111/api/v1/products";
             var dados = new { name = Name, description = Description, price = Price };
 
             // Converter os dados para JSON
             var json = JsonConvert.SerializeObject(dados);
             var conteudo = new StringContent(json, Encoding.UTF8, "application/json");
-            var httpClient = new HttpClient();
-            // Enviar a requisição POST
+            
             var resposta = await httpClient.PostAsync(url, conteudo);
 
             if (!resposta.IsSuccessStatusCode)
             {
-                // A requisição falhou, trate o erro de acordo com suas necessidades
-                // Você pode acessar a mensagem de erro através de resposta.ReasonPhrase
                 ModelState.AddModelError(string.Empty, "Erro ao chamar a API.");
                 return Page();
             }
 
-            var ultImputImage = "https://localhost:7111/api/v1/images/upload";
-
-            var image = new { ProductId = "6a82fa49-c820-49a5-8055-fc8a66b78ab1", File = FileInput.OpenReadStream() };
-            var jsonImage = JsonConvert.SerializeObject(image);
-            var conteudoImage = new StringContent(jsonImage, Encoding.UTF8, "application/json");
-
-            var respostaImage = await httpClient.PostAsync(ultImputImage, conteudoImage);
-
-
-            return RedirectToPage("/Index");
-        }
-        private byte[] SerializeFile(IFormFile arquivo)
-        {
-            using (var memoryStream = new MemoryStream())
+            var apiUrl = "https://localhost:7111/api/v1/images/upload";
+            using (var form = new MultipartFormDataContent())
             {
-                arquivo.CopyTo(memoryStream);
-                return memoryStream.ToArray();
+                using (var fileStream = FileInput.OpenReadStream())
+                {
+                    form.Add(new StringContent("6a82fa49-c820-49a5-8055-fc8a66b78ab1"), "ProductId");
+                    form.Add(new StreamContent(fileStream), "file", FileInput.FileName);
+
+                    using (var response = await httpClient.PostAsync(apiUrl, form))
+                    {
+                        // Trate a resposta da chamada REST como desejado
+                        if (response.IsSuccessStatusCode)
+                        {
+                            return RedirectToPage("/Index");
+                        }
+                        else
+                        {
+                            ModelState.AddModelError(string.Empty, "Erro ao chamar a API.");
+                            return Page();
+                        }
+                    }
+                }
             }
         }
     }
